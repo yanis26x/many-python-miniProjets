76bdb9b754fa402fc1f156cba9d9fac57ee095f8
yanis djenadi <113249548+yanis26x@users.noreply.github.com>
2025-04-02 15:11:24 -0400

    Update senderr.py

--- senderr.py
+++ senderr.py
@@ -4,3 +4,5 @@ import time
 
 now = datetime.datetime.now()
 kit.sendwhatmsg("+15142626266", "Cool Message sent via Python 26xxxxxx", now.hour, now.minute + 1)
+# Same as above but Closes the Tab in 2 Seconds after Sending the Message
+pywhatkit.sendwhatmsg("+910123456789", "Salut! ", 13, 30, 15, True, 2)
